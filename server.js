c9304d6ab58dada14749ed6e6ce018c8544d0b17
Peter <peterkimm35@gmail.com>
2022-05-16 21:47:53 -0700

    Tip Calculator express application created

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ app.get("/greeting/:name", (req, res) => {
     res.send("Hello" + " " + req.params.name)
   })
 
+  app.get("/tip/:total/:tipPercentage", (req, res) => {
+      res.send("Your tip is" + " " + req.params.total * (req.params.tipPercentage * 0.01) + " " + "dollars")
+  })
+
 app.listen(3000, () => {
     console.log("Express is listening")
 });
+
